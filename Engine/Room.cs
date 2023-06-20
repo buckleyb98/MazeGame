9d23bb3eefda99c4e284843fd5ee0ed099649912
Billyjack Buckley <buckleyb1998@hotmail.co.uk>
2023-06-20 19:38:03 +0100

    Added Quest class with properties and constructors Added custom datatypes for Room, Quest, Item and Monster

--- Engine/Room.cs
+++ Engine/Room.cs
@@ -11,12 +11,23 @@ namespace Engine
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Item EntryItemRequired { get; set; }
+        public Quest IsQuestHere { get; set; }
+        public Monster IsMonsterHere { get; set; }
+        public Room RoomToNorth { get; set; }
+        public Room RoomToSouth { get; set; }
+        public Room RoomToEast { get; set; }
+        public Room RoomToWest { get; set; }
 
-        public Room(int id, string name, string description)
+        public Room(int id, string name, string description, Item entryItemRequired = null, 
+            Quest isQuestHere = null, Monster isMonsterHere = null)
         {
             ID = id;
             Name = name;
             Description = description;
+            EntryItemRequired = entryItemRequired;
+            IsQuestHere = isQuestHere;
+            IsMonsterHere = isMonsterHere;
         }
     }
 }
